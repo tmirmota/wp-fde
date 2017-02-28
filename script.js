4fb720094f6c99423dab16ff45ae0b5c536ed9cc
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-02-27 20:57:44 -0800

    took out css files

---
+++ script.js
@@ -0,0 +1,17 @@
+<h2 style="color: #fff;">Subscribe to our Newsletter</h2>
+<div id="footer-subscription"></div>
+<!--[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]-->
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js">
+    $(window).load(function(){
+      console.log('test');
+      hbspt.forms.create({
+        css: '',
+        portalId: '2116999',
+        target: '#footer-subscription',
+        formId: '98b51171-c2ff-4587-9857-6262e08d7740',
+        formInstanceId: '1'
+      });
+    });
+</script>
